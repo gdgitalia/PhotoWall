0dd42e23327bf7fdff6d6e3e69d86f9424094056
Patrick Arminio <patrick.arminio@gmail.com>
2017-05-15 23:01:41 -0700

    Use rsz.io to resize images

--- src/pages/wall.js
+++ src/pages/wall.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import firebase from "firebase";
 
+const getImageSrc = src => src.replace(".com", ".com.rsz.io") + "?width=400";
+
 class Wall extends Component {
   state = {
     images: []
@@ -40,7 +42,7 @@ class Wall extends Component {
       <div className="wall">
         {images.map(src => (
           <div className="wall__image-wrapper" key={src}>
-            <img className="wall__image" src={src} />
+            <img className="wall__image" src={getImageSrc(src)} />
           </div>
         ))}
       </div>
