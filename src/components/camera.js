c79d89cbed3f5b653d612ae6dad9740e984ab0d8
Patrick Arminio <patrick.arminio@gmail.com>
2017-05-16 00:50:37 -0700

    Fix orientation issue on ios

--- src/components/camera.js
+++ src/components/camera.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Webcam from "react-webcam";
+import getOrientedImage from "exif-orientation-image";
 
 import { uploadImage } from "../utils/firebase";
 
@@ -22,6 +23,54 @@ class Camera extends React.Component {
     });
   };
 
+  _resize = file => {
+    const promise = new Promise((resolve, reject) => {
+      const img = document.createElement("img");
+      const canvas = document.createElement("canvas");
+      const reader = new FileReader();
+
+      getOrientedImage(file, function(err, canvas) {
+        if (err) {
+          console.error(err);
+
+          return;
+        }
+
+        const MAX_WIDTH = 800;
+        const MAX_HEIGHT = 600;
+        let width = canvas.width;
+        let height = canvas.height;
+
+        if (width > height) {
+          if (width > MAX_WIDTH) {
+            height *= MAX_WIDTH / width;
+            width = MAX_WIDTH;
+          }
+        } else {
+          if (height > MAX_HEIGHT) {
+            width *= MAX_HEIGHT / height;
+            height = MAX_HEIGHT;
+          }
+        }
+
+        img.src = canvas.toDataURL("image/jpg");
+
+        img.onload = () => {
+          canvas.width = width;
+          canvas.height = height;
+
+          const ctx = canvas.getContext("2d");
+          ctx.drawImage(img, 0, 0, width, height);
+
+          resolve(canvas.toDataURL("image/jpg"));
+        };
+      });
+
+      reader.readAsDataURL(file);
+    });
+    return promise;
+  };
+
   handleCapture = () => {
     const imageSrc = this._camera.getScreenshot();
 
@@ -39,15 +88,11 @@ class Camera extends React.Component {
       return;
     }
 
-    const reader = new FileReader();
-
-    reader.addEventListener("load", () => this._upload(reader.result), false);
+    this._resize(file).then(data => this._upload(data));
 
     this.setState({
       uploading: true
     });
-
-    reader.readAsDataURL(file);
   };
 
   render() {
